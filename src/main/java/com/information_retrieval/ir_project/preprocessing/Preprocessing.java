cec452228ddb2f03bd4743193dc626eb4baa2ed5
abdo <magdyabdo@gmail.com>
2024-05-03 23:03:27 +0300

    preprocessing impl.

---
+++ src/main/java/com/information_retrieval/ir_project/preprocessing/Preprocessing.java
@@ -0,0 +1,43 @@
+package com.information_retrieval.ir_project.preprocessing;
+
+import java.util.ArrayList;
+
+
+public class Preprocessing {
+
+    public static ArrayList<String> preprocess(String str,
+            boolean stopWords, boolean normalization, boolean Stemming, boolean lemma) throws Exception {
+        TextCleaning textCleaning = new TextCleaning();
+        ArrayList<String> tokens = textCleaning.Tokenization(str);
+        if(stopWords)
+            tokens=textCleaning.stopWords(tokens);
+        if (normalization) {
+            tokens = textCleaning.normailzation(tokens);
+        }
+        if(Stemming){
+            PorterStemmer stemmer = new PorterStemmer();
+            ArrayList<String> stemOut=new ArrayList<>();
+          for(String words  : tokens){
+               stemmer.add(words.toCharArray(), words.length());
+               stemmer.stem();
+               String out = new String(stemmer.getResultBuffer(), 0, stemmer.getResultLength());
+               stemOut.add(out);
+          }
+          tokens=stemOut;
+        }
+        if(lemma){
+             tokens=StemLemmatize.lemmatizeTokens(tokens);
+        }
+        tokens.removeIf((String x)->x.equals("O"));
+        return tokens;
+    }
+
+    /*
+     * Tokenization ✅
+     * StopWords ✅
+     * Normalization ✅
+     * Stemming ✅
+     * Lemma ✅
+     * */
+}
+
---
+++ src/main/java/com/information_retrieval/ir_project/preprocessing/StemLemmatize.java
@@ -0,0 +1,42 @@
+
+package com.information_retrieval.ir_project.preprocessing;
+
+
+
+
+import opennlp.tools.lemmatizer.DictionaryLemmatizer;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StemLemmatize {
+
+    public static ArrayList<String> lemmatizeTokens(ArrayList<String> inputTokens) throws Exception {
+
+        String[] tokens=new String[inputTokens.size()];
+        int count=0;
+        for (String inputToken : inputTokens) {
+            tokens[count++]=inputToken;
+        }
+
+        InputStream inputStreamPOSTagger = new FileInputStream("C:\\Users\\Abdo\\Desktop\\Ir\\Information-Retrieval-Project\\archive\\en-pos-maxent.bin");
+        POSModel posModel = new POSModel(inputStreamPOSTagger);
+        POSTaggerME posTagger = new POSTaggerME(posModel);
+        String tags[] = posTagger.tag(tokens);
+        InputStream dictLemmatizer = new FileInputStream("C:\\Users\\Abdo\\Desktop\\Ir\\Information-Retrieval-Project\\archive\\en-lemmatizer.dict");
+        DictionaryLemmatizer lemmatizer = new DictionaryLemmatizer(
+                dictLemmatizer);
+        String[] lemmas = lemmatizer.lemmatize(tokens, tags);
+        for(int i=0;i<lemmas.length;i++){
+            if(lemmas[i].equals("O")){
+                lemmas[i] = inputTokens.get(i);
+            }
+        }
+        return new ArrayList<>(Arrays.asList(lemmas));
+    }
+
+}
