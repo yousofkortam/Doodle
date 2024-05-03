7b261f3618767617bd421b842020b81ed0c43e9d
abdo <magdyabdo@gmail.com>
2024-05-03 23:04:45 +0300

    Add required algorithms and prepocessing

---
+++ src/main/java/com/information_retrieval/ir_project/IrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.information_retrieval.ir_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/information_retrieval/ir_project/IrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.information_retrieval.ir_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
