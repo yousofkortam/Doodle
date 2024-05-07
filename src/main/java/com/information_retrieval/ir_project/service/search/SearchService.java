04abf47fec836f5f41aab04e75c1c4baf7c05d7c
sameh-tarek <tarksame7@gmail.com>
2024-05-07 20:00:34 +0300

    Add implementation of Search Service

--- src/main/java/com/information_retrieval/ir_project/service/search/SearchService.java
+++ src/main/java/com/information_retrieval/ir_project/service/search/SearchService.java
@@ -1,5 +1,7 @@
 package com.information_retrieval.ir_project.service.search;
 
+import java.util.List;
+
 public interface SearchService {
-    String search(String indexType, String searchText, boolean normalization, boolean steaming, boolean lemetization);
+    String search(String indexType, String searchText, boolean normalization, boolean steaming, boolean lemmatization);
 }
