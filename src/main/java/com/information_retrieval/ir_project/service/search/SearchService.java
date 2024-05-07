6e158fa99b70bef66743a6980ed89b2914780370
yousofkortam <yousofkortam@outlook.com>
2024-05-07 04:26:12 +0300

    chore: Implement better user experience

--- src/main/java/com/information_retrieval/ir_project/service/search/SearchService.java
+++ src/main/java/com/information_retrieval/ir_project/service/search/SearchService.java
@@ -1,5 +1,5 @@
 package com.information_retrieval.ir_project.service.search;
 
 public interface SearchService {
-    public String search(String indexType, String searchText, boolean normalization, boolean steaming, boolean lemetization);
+    String search(String indexType, String searchText, boolean normalization, boolean steaming, boolean lemetization);
 }
