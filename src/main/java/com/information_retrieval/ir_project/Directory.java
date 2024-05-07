b5f7a2a1bd29181383a24f1f41fa3908c424c7cc
abdo <magdyabdo@gmail.com>
2024-05-07 18:51:02 +0300

    update directory path

--- src/main/java/com/information_retrieval/ir_project/Directory.java
+++ src/main/java/com/information_retrieval/ir_project/Directory.java
@@ -1,7 +1,12 @@
 package com.information_retrieval.ir_project;
 
+import org.springframework.stereotype.Component;
+
 public class Directory {
 
-    public static String ARCHIVE_PATH = "../../../archive";
+    public static String ARCHIVE_PATH ;
 
+    public static void setArchivePath(String archivePath) {
+        ARCHIVE_PATH = archivePath;
+    }
 }
